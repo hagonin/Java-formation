877d98e84633c83a424367b4c182e653f2598628
hagonin <goninha@outlook.fr>
2025-07-07 13:52:31 +0200

    declaration variable

---
+++ src/DeclarationApp.java
@@ -0,0 +1,17 @@
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = 12;
+        short b = 2345;
+        int c = 2345;
+        long d = 23453578L;
+        float f = 2345f;
+        double d1 = 2345d;
+
+        char caractereUnicode = '\u09a3';
+        boolean val = false;
+        String chaine = "Hello";
+        String randomString = "Voici le résultat d'un calcul : \n3 + 6 = 9";
+
+        System.out.println(randomString);
+    }
+}
